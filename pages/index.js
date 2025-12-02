7c04293c80058d676cb6155ea9f706f1bbeb91af
ThalesBonel <thalesbbonel@gmail.com>
2025-12-02 08:45:04 -0300

    Ajuste div

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 function Home() {
   return (
-    (<h1>Projeto FUTURO - AporteNow!!!!</h1>),
-    (
+    <div>
+      <h1>Projeto FUTURO - AporteNow!!!!</h1>
+
       <p>
         Aqui será feito um projeto de Rebalanceamento de Carteira de
         Investimentos
       </p>
-    ),
-    (
+
       <p>
         Onde as pessoas poderão confiar, a partir de sua notas para cada tipo de
         ação, que sua carteira estará SEMPRE balanceada
       </p>
-    )
+    </div>
   );
 }
 
