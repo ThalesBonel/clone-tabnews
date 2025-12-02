1da3a1aed4dd9581b64a0a5e120f776a3640ddee
ThalesBonel <thalesbbonel@gmail.com>
2025-12-02 08:41:00 -0300

    adicionando mensagem OBJETIVO do projeto - Aportenow

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>MEU AMOOR, EU TE AMOO MUITOOO!!! PRA SEMPRE!</h1>;
+  return (
+    (<h1>Projeto FUTURO - AporteNow!!!!</h1>),
+    (
+      <p>
+        Aqui será feito um projeto de Rebalanceamento de Carteira de
+        Investimentos
+      </p>
+    ),
+    (
+      <p>
+        Onde as pessoas poderão confiar, a partir de sua notas para cada tipo de
+        ação, que sua carteira estará SEMPRE balanceada
+      </p>
+    )
+  );
 }
 
 export default Home;
